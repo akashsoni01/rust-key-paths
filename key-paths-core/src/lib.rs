690a6bba27f1cde967558fce46bb0fcc1accebb8
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-08-24 18:46:37 +0530

    trait added

--- key-paths-core/src/lib.rs
+++ key-paths-core/src/lib.rs
@@ -9,3 +9,33 @@ pub struct WritableKeyPath<Root, Value> {
     pub get_mut: for<'a> fn(&'a mut Root) -> &'a mut Value,
 }
 
+/// Common trait for readable keypaths
+pub trait Readable<Root, Value> {
+    fn get<'a>(&self, root: &'a Root) -> &'a Value;
+
+    fn iter<'a>(&self, slice: &'a [Root]) -> Box<dyn Iterator<Item = &'a Value> + 'a>
+    where
+        Self: Sized,
+    {
+        let f = self.get_fn(); // capture fn pointer
+        Box::new(slice.iter().map(move |root| f(root)))
+    }
+
+    fn get_fn(&self) -> for<'a> fn(&'a Root) -> &'a Value;
+}
+
+/// Extra trait for writable keypaths
+pub trait Writable<Root, Value>: Readable<Root, Value> {
+    fn get_mut<'a>(&self, root: &'a mut Root) -> &'a mut Value;
+
+    fn iter_mut<'a>(&self, slice: &'a mut [Root]) -> Box<dyn Iterator<Item = &'a mut Value> + 'a>
+    where
+        Self: Sized,
+    {
+        let f = self.get_mut_fn(); // capture fn pointer
+        Box::new(slice.iter_mut().map(move |root| f(root)))
+    }
+
+    fn get_mut_fn(&self) -> for<'a> fn(&'a mut Root) -> &'a mut Value;
+}
+
