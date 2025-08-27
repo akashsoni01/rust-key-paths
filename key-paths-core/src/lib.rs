fdd85767841d91ae519582791a0f4daa5ca00127
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-08-27 16:23:36 +0530

    doc wip

--- key-paths-core/src/lib.rs
+++ key-paths-core/src/lib.rs
@@ -1,17 +1,3 @@
-// pub trait Readable<Root, Value> {
-//     fn get<'a>(&self, root: &'a Root) -> &'a Value;
-//
-//     fn iter<'a>(&self, slice: &'a [Root]) -> Box<dyn Iterator<Item = &'a Value> + 'a>
-//     where
-//         Self: Sized,
-//     {
-//         let f = self.get_fn(); // capture fn pointer
-//         Box::new(slice.iter().map(move |root| f(root)))
-//     }
-//
-//     fn get_fn(&self) -> for<'a> fn(&'a Root) -> &'a Value;
-// }
-
 /// Read-only keypath
 pub struct ReadableKeyPath<Root, Value> {
     pub get: Box<dyn for<'a> Fn(&'a Root) -> &'a Value>,
@@ -27,8 +13,6 @@ impl<Root, Value> ReadableKeyPath<Root, Value> {
 
 }
 
-// --- Compose: impl over (Root, Mid); Value is method-generic ---
-// --- Compose ---
 impl<Root, Mid> ReadableKeyPath<Root, Mid>
 where
     Root: 'static,
@@ -47,23 +31,6 @@ where
         })
     }
 }
-// impl<Root, Value> ReadableKeyPath<Root, Value> {
-//     pub fn new(get: for<'a> fn(&'a Root) -> &'a Value) -> Self {
-//         Self { get }
-//     }
-// }
-// 
-// impl<Root, Value> ReadableKeyPath<Root, Value> {
-//     pub fn get<'a>(&self, root: &'a Root) -> &'a Value {
-//         (self.get)(root)
-//     }
-// 
-//     // pub fn get_fn(&self) -> for<'a> fn(&'a Root) -> &'a Value {
-//     //     self.get
-//     // }
-// }
-// 
-
 
 pub struct WritableKeyPath<Root, Value> {
     pub get_mut: Box<dyn for<'a> Fn(&'a mut Root) -> &'a mut Value>,
@@ -100,58 +67,6 @@ where
     }
 }
 
-/// Read/write keypath
-// pub struct WritableKeyPath<Root, Value> {
-//     pub get_mut: for<'a> fn(&'a mut Root) -> &'a mut Value,
-// }
-
-// pub trait Writable<Root, Value>: Readable<Root, Value> {
-//     fn get_mut<'a>(&self, root: &'a mut Root) -> &'a mut Value;
-//
-//     fn iter_mut<'a>(&self, slice: &'a mut [Root]) -> Box<dyn Iterator<Item = &'a mut Value> + 'a>
-//     where
-//         Self: Sized,
-//     {
-//         let f = self.get_mut_fn(); // capture fn pointer
-//         Box::new(slice.iter_mut().map(move |root| f(root)))
-//     }
-//
-//     fn get_mut_fn(&self) -> for<'a> fn(&'a mut Root) -> &'a mut Value;
-// }
-
-// impl<Root, Value> WritableKeyPath<Root, Value> {
-//     pub fn new(
-//         get_mut: for<'a> fn(&'a mut Root) -> &'a mut Value,
-//     ) -> Self {
-//         Self { get_mut }
-//     }
-// }
-
-// impl<Root, Value> WritableKeyPath<Root, Value> {
-//     pub fn get<'a>(&self, root: &'a mut Root) -> &'a mut Value {
-//         (self.get_mut)(root)
-//     }
-// 
-//     fn get_fn(&self) -> for<'a> fn(&'a Root) -> &'a Value {
-//         self.get
-//     }
-// }
-
-// impl<Root, Value> WritableKeyPath<Root, Value> {
-//     pub fn get_mut<'a>(&self, root: &'a mut Root) -> &'a mut Value {
-//         (self.get_mut)(root)
-//     }
-// 
-//     // pub fn get_mut_fn(&self) -> for<'a> fn(&'a mut Root) -> &'a mut Value {
-//     //     self.get_mut
-//     // }
-// }
-
-// pub trait FailableReadable<Root, Value> {
-//     fn try_get<'a>(&self, root: &'a Root) -> Option<&'a Value>;
-// }
-
-// --- Core type ---
 pub struct FailableReadableKeyPath<Root, Value> {
     pub get: Box<dyn for<'a> Fn(&'a Root) -> Option<&'a Value>>,
 }
@@ -166,7 +81,6 @@ impl<Root, Value> FailableReadableKeyPath<Root, Value> {
 
 }
 
-// --- Compose: impl over (Root, Mid); Value is method-generic ---
 impl<Root, Mid> FailableReadableKeyPath<Root, Mid>
 where
     Root: 'static,
@@ -199,7 +113,6 @@ impl<Root, Value> FailableWritableKeyPath<Root, Value> {
     }
 }
 
-// ---------------- COMPOSE ----------------
 impl<Root, Mid> FailableWritableKeyPath<Root, Mid>
 where
     Root: 'static,
@@ -217,8 +130,6 @@ where
         })
     }
 }
-
-
 
 pub struct EnumKeyPath<Enum, Inner> {
     pub extract: fn(&Enum) -> Option<&Inner>,
@@ -238,7 +149,6 @@ impl<Enum, Inner> EnumKeyPath<Enum, Inner> {
         (self.embed)(inner)
     }
 }
-
 
 #[macro_export]
 macro_rules! enum_keypath {
@@ -270,21 +180,23 @@ macro_rules! enum_keypath {
     }};
 }
 
-/// Macro for readable keypaths
-#[macro_export]
-macro_rules! readable_keypath {
-    ($Root:ty, $field:ident) => {
-        ReadableKeyPath::new(|root: &$Root| &root.$field)
-    };
-}
 
-/// Macro for writable keypaths
-#[macro_export]
-macro_rules! writable_keypath {
-    ($Root:ty, $field:ident) => {
-        WritableKeyPath::new(
-            |root: &$Root| &root.$field,
-            |root: &mut $Root| &mut root.$field,
-        )
-    };
-}
+// 
+// /// Macro for readable keypaths
+// #[macro_export]
+// macro_rules! readable_keypath {
+//     ($Root:ty, $field:ident) => {
+//         ReadableKeyPath::new(|root: &$Root| &root.$field)
+//     };
+// }
+// 
+// /// Macro for writable keypaths
+// #[macro_export]
+// macro_rules! writable_keypath {
+//     ($Root:ty, $field:ident) => {
+//         WritableKeyPath::new(
+//             |root: &$Root| &root.$field,
+//             |root: &mut $Root| &mut root.$field,
+//         )
+//     };
+// }
