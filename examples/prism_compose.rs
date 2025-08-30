b46c7434d32c25a48d7cd38bb5f2e65eb86be7b1
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-08-30 14:12:34 +0530

    compose with enums

---
+++ examples/prism_compose.rs
@@ -0,0 +1,72 @@
+use key_paths_core::KeyPaths;
+
+#[derive(Debug)]
+struct Size {
+    width: u32,
+    height: u32,
+}
+
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Other(RGBU8),
+}
+
+#[derive(Debug)]
+struct RGBU8(u8, u8, u8);
+
+#[derive(Debug)]
+struct ABox {
+    name: String,
+    size: Size,
+    color: Color,
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    size: Size,
+    name: String,
+}
+
+
+
+fn main() {
+    let mut a_box = ABox {
+        name: String::from("A box"),
+        size: Size {
+            width: 10,
+            height: 20,
+        },
+        color: Color::Other(
+            RGBU8(10, 20, 30)
+        ),
+    };
+
+    let color_kp: KeyPaths<ABox, Color> = KeyPaths::failable_writable(|x: &mut ABox| Some(&mut x.color));
+    
+    
+    let case_path = KeyPaths::prism_mut(
+        {
+            |v| Color::Other(v)
+        },
+        |p: &Color| match p {
+            Color::Other(rgb) => Some(rgb),
+            _ => None,
+        },
+        |p: &mut Color| match p {
+            Color::Other(rgb) => Some(rgb),
+            _ => None,
+        },
+
+    );
+    
+    // let's compose color with rgb 
+    
+    println!("{:?}", a_box);
+    let color_rgb_kp = color_kp.compose(case_path);
+    if let Some(value) = color_rgb_kp.get_mut(&mut a_box) {
+        *value = RGBU8(10, 20, 30);
+    }
+}
