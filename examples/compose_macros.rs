65755c4f6937534424ad8a07b678e5da3bf14408
Akash Soni <akashsoni8285@gmail.com>
2025-09-18 18:32:54 +0530

    optional support added in macros

---
+++ examples/compose_macros.rs
@@ -0,0 +1,64 @@
+use key_paths_core::KeyPaths;
+use key_paths_derive::Keypaths;
+
+#[derive(Debug, Keypaths)]
+struct Engine {
+    horsepower: u32,
+}
+
+#[derive(Debug, Keypaths)]
+struct Car {
+    engine: Option<Engine>,
+}
+
+#[derive(Debug, Keypaths)]
+struct Garage {
+    car: Option<Car>,
+}
+
+#[derive(Debug, Keypaths)]
+struct City {
+    garage: Option<Garage>,
+}
+
+fn main() {
+    let city = City {
+        garage: Some(Garage {
+            car: Some(Car {
+                engine: Some(Engine { horsepower: 250 }),
+            }),
+        }),
+    };
+
+    // Compose using derive-generated failable readable methods
+    let city_hp = City::garage_fr()
+        .compose(Garage::car_fr())
+        .compose(Car::engine_fr())
+        .compose(Engine::horsepower_fr());
+
+    println!("Horsepower = {:?}", city_hp.get(&city));
+
+    // Demonstrate writable/failable-writable compose
+    let mut city2 = City {
+        garage: Some(Garage { car: Some(Car { engine: Some(Engine { horsepower: 100 }) }) }),
+    };
+
+    let garage_fw = City::garage_fw();
+    let car_fw = Garage::car_fw();
+    let engine_fw = Car::engine_fw();
+    let hp_fw = Engine::horsepower_fw();
+
+    if let Some(garage) = garage_fw.get_mut(&mut city2) {
+        if let Some(car) = car_fw.get_mut(garage) {
+            if let Some(engine) = engine_fw.get_mut(car) {
+                if let Some(hp) = hp_fw.get_mut(engine) {
+                    *hp += 23;
+                }
+            }
+        }
+    }
+
+    println!("Updated city2 = {:?}", city2);
+}
+
+
--- key-paths-derive/src/lib.rs
+++ key-paths-derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
 
 #[proc_macro_derive(Keypaths)]
 pub fn derive_keypaths(input: TokenStream) -> TokenStream {
@@ -21,20 +21,43 @@ pub fn derive_keypaths(input: TokenStream) -> TokenStream {
                     let fr_fn = format_ident!("{}_fr", field_ident);
                     let fw_fn = format_ident!("{}_fw", field_ident);
 
-                    tokens.extend(quote! {
-                        pub fn #r_fn() -> key_paths_core::KeyPaths<#name, #ty> {
-                            key_paths_core::KeyPaths::readable(|s: &#name| &s.#field_ident)
+                    // Helper: detect Option<T>
+                    let option_inner: Option<Type> = extract_option_inner_type(ty);
+
+                    match option_inner {
+                        Some(inner_ty) => {
+                            tokens.extend(quote! {
+                                pub fn #r_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::readable(|s: &#name| &s.#field_ident)
+                                }
+                                pub fn #w_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::writable(|s: &mut #name| &mut s.#field_ident)
+                                }
+                                pub fn #fr_fn() -> key_paths_core::KeyPaths<#name, #inner_ty> {
+                                    key_paths_core::KeyPaths::failable_readable(|s: &#name| s.#field_ident.as_ref())
+                                }
+                                pub fn #fw_fn() -> key_paths_core::KeyPaths<#name, #inner_ty> {
+                                    key_paths_core::KeyPaths::failable_writable(|s: &mut #name| s.#field_ident.as_mut())
+                                }
+                            });
                         }
-                        pub fn #w_fn() -> key_paths_core::KeyPaths<#name, #ty> {
-                            key_paths_core::KeyPaths::writable(|s: &mut #name| &mut s.#field_ident)
+                        None => {
+                            tokens.extend(quote! {
+                                pub fn #r_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::readable(|s: &#name| &s.#field_ident)
+                                }
+                                pub fn #w_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::writable(|s: &mut #name| &mut s.#field_ident)
+                                }
+                                pub fn #fr_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::failable_readable(|s: &#name| Some(&s.#field_ident))
+                                }
+                                pub fn #fw_fn() -> key_paths_core::KeyPaths<#name, #ty> {
+                                    key_paths_core::KeyPaths::failable_writable(|s: &mut #name| Some(&mut s.#field_ident))
+                                }
+                            });
                         }
-                        pub fn #fr_fn() -> key_paths_core::KeyPaths<#name, #ty> {
-                            key_paths_core::KeyPaths::failable_readable(|s: &#name| Some(&s.#field_ident))
-                        }
-                        pub fn #fw_fn() -> key_paths_core::KeyPaths<#name, #ty> {
-                            key_paths_core::KeyPaths::failable_writable(|s: &mut #name| Some(&mut s.#field_ident))
-                        }
-                    });
+                    }
                 }
                 tokens
             }
@@ -54,6 +77,24 @@ pub fn derive_keypaths(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
+}
+
+fn extract_option_inner_type(ty: &Type) -> Option<Type> {
+    use syn::{PathArguments, GenericArgument};
+    if let Type::Path(tp) = ty {
+        if let Some(seg) = tp.path.segments.last() {
+            if seg.ident == "Option" {
+                if let PathArguments::AngleBracketed(ab) = &seg.arguments {
+                    for arg in ab.args.iter() {
+                        if let GenericArgument::Type(inner) = arg {
+                            return Some(inner.clone());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    None
 }
 
 
