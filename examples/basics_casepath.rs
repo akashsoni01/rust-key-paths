a0aca2d296a3bf961b8bd99130db7a560d35126b
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-11-02 13:14:34 +0530

    wip

--- examples/basics_casepath.rs
+++ examples/basics_casepath.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
+use parking_lot::RwLock;
 use key_paths_derive::{Casepaths, Keypaths};
 
 #[derive(Debug, Keypaths)]
 struct SomeComplexStruct {
     scsf: Option<SomeOtherStruct>,
+    scfs2: Arc<RwLock<SomeOtherStruct>>
 }
-
 
 #[derive(Debug, Keypaths)]
 struct SomeOtherStruct {
@@ -14,7 +16,7 @@ struct SomeOtherStruct {
 #[derive(Debug, Casepaths)]
 enum SomeEnum {
     A(String),
-    B(DarkStruct),
+    B(Box<DarkStruct>),
 }
 
 #[derive(Debug, Keypaths)]
@@ -35,11 +37,23 @@ impl SomeComplexStruct {
             scsf: Some(SomeOtherStruct {
                 sosf: Some(OneMoreStruct {
                     omsf: Some(String::from("no value for now")),
-                    omse: Some(SomeEnum::B(DarkStruct {
+                    omse: Some(SomeEnum::B(Box::new(DarkStruct {
                         dsf: Some(String::from("dark field")),
-                    })),
+                    }))),
                 }),
             }),
+            scfs2: Arc::new(
+                RwLock::new(
+                    SomeOtherStruct {
+                        sosf: Some(OneMoreStruct {
+                            omsf: Some(String::from("no value for now")),
+                            omse: Some(SomeEnum::B(Box::new(DarkStruct {
+                                dsf: Some(String::from("dark field")),
+                            }))),
+                        }),
+                    }
+                )
+            )
         }
     }
 }
@@ -48,17 +62,16 @@ fn main() {
         .then(SomeOtherStruct::sosf_fw())
         .then(OneMoreStruct::omse_fw())
         .then(SomeEnum::b_case_w())
-        .then(DarkStruct::dsf_fw());
+        .then(DarkStruct::dsf_fw().for_box());
 
     let mut instance = SomeComplexStruct::new();
     // let omsf = dsf_kp.get_mut(&mut instance);
     // *omsf.unwrap() =
     //     String::from("we can change the field with the other way unlocked by keypaths");
     // println!("instance = {:?}", instance);
-
     if let Some(omsf) = dsf_kp.get_mut(&mut instance) {
         *omsf = String::from("This is changed 🖖🏿");
         println!("instance = {:?}", instance);
-
+    
     }
 }
