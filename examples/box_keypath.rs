ba29ba4aa26dceb04a7fb73d8474acb0994ba80f
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-12-06 20:06:20 +0530

    box working

--- examples/box_keypath.rs
+++ examples/box_keypath.rs
@@ -1,7 +1,7 @@
-use key_paths_derive::{Casepaths, Keypaths};
+use keypaths_proc::{Casepaths, Keypaths};
 
 #[derive(Debug, Keypaths)]
-#[All]
+#[Writable]
 struct SomeComplexStruct {
     scsf: Box<SomeOtherStruct>,
 }
@@ -22,39 +22,54 @@ impl SomeComplexStruct {
 }
 
 #[derive(Debug, Keypaths)]
-#[All]
+#[Writable]
 struct SomeOtherStruct {
     sosf: OneMoreStruct,
 }
 
 #[derive(Debug, Casepaths)]
+#[Writable]
 enum SomeEnum {
     A(String),
     B(DarkStruct),
 }
 
 #[derive(Debug, Keypaths)]
-#[All]
+#[Writable]
 struct OneMoreStruct {
     omsf: String,
     omse: SomeEnum,
 }
 
 #[derive(Debug, Keypaths)]
-#[All]
+#[Writable]
 struct DarkStruct {
     dsf: String,
 }
 
 fn main() {
-    let op = SomeComplexStruct::scsf_fw()
-        .then(SomeOtherStruct::sosf_fw())
-        .then(OneMoreStruct::omse_fw())
-        .then(SomeEnum::b_case_w())
-        .then(DarkStruct::dsf_fw());
+    use rust_keypaths::WritableOptionalKeyPath;
+    
+    // Note: These fields are NOT Option types, so we use _w() methods, not _fw()
+    // For Box<T>, we manually create a keypath that unwraps the Box
+    // For enum variants, we use _case_fw() which returns WritableOptionalKeyPath
+    
+    // Manually create keypath to unwrap Box<SomeOtherStruct>
+    let box_unwrap = WritableOptionalKeyPath::new(|s: &mut SomeComplexStruct| {
+        Some(&mut *s.scsf)  // Dereference Box to get &mut SomeOtherStruct
+    });
+    
+    let op = box_unwrap
+        .then(SomeOtherStruct::sosf_fw())  // Convert to OptionalKeyPath for chaining
+        .then(OneMoreStruct::omse_w().to_optional())  // Convert to OptionalKeyPath for chaining
+        .then(SomeEnum::b_case_fw())  // Enum variant returns WritableOptionalKeyPath
+        .then(DarkStruct::dsf_w().to_optional());  // Convert to OptionalKeyPath for chaining
+    
     let mut instance = SomeComplexStruct::new();
-    let omsf = op.get_mut(&mut instance);
-    **omsf =
-        String::from("we can change the field with the other way unlocked by keypaths");
-    println!("instance = {:?}", instance);
+    
+    // get_mut() returns Option<&mut String> for WritableOptionalKeyPath
+    if let Some(omsf) = op.get_mut(&mut instance) {
+        *omsf = String::from("we can change the field with the other way unlocked by keypaths");
+        println!("instance = {:?}", instance);
+    }
 }
