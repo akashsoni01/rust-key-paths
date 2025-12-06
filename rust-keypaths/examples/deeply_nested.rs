28e393045688f3419b0fc850987d7ce173c1af2b
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-12-06 15:10:06 +0530

    wip eg

---
+++ rust-keypaths/examples/deeply_nested.rs
@@ -0,0 +1,62 @@
+use key_paths_derive::{Casepaths, Keypaths};
+
+#[derive(Debug, Keypaths)]
+#[Writable] // Default scope for every field is Readable, others Writable, Owned and All.
+struct SomeComplexStruct {
+    scsf: Option<SomeOtherStruct>,
+}
+
+#[derive(Debug, Keypaths)]
+struct SomeOtherStruct {
+    sosf: Option<OneMoreStruct>,
+}
+
+#[derive(Debug, Keypaths)]
+#[Writable] // Default scope for every field is Readable, others Writable, Owned and All.
+struct OneMoreStruct {
+    omsf: Option<String>,
+    omse: Option<SomeEnum>,
+}
+
+#[derive(Debug, Casepaths)]
+enum SomeEnum {
+    A(String),
+    B(DarkStruct),
+}
+
+#[derive(Debug, Keypaths)]
+#[Writable] // Default scope for every field is Readable, others Writable, Owned and All.
+struct DarkStruct {
+    dsf: Option<String>,
+}
+
+impl SomeComplexStruct {
+    fn new() -> Self {
+        Self {
+            scsf: Some(SomeOtherStruct {
+                sosf: Some(OneMoreStruct {
+                    omsf: Some(String::from("no value for now")),
+                    omse: Some(SomeEnum::B(DarkStruct {
+                        dsf: Some(String::from("dark field")),
+                    })),
+                }),
+            }),
+        }
+    }
+}
+
+fn main() {
+    let dsf_kp = SomeComplexStruct::scsf_fw()
+        .then(SomeOtherStruct::sosf_fw())
+        .then(OneMoreStruct::omse_fw())
+        .then(SomeEnum::b_case_w())
+        .then(DarkStruct::dsf_fw());
+
+    let mut instance = SomeComplexStruct::new();
+    
+    if let Some(omsf) = dsf_kp.get_mut(&mut instance) {
+        *omsf = String::from("This is changed 🖖🏿");
+        println!("instance = {:?}", instance);
+    }
+}
+
