79b4facea5a7828f07b2078a1fb519e36fa34bbe
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-12-15 23:13:54 +0530

    iter working

--- examples/iters.rs
+++ examples/iters.rs
@@ -1,17 +1,18 @@
-use rust_keypaths::{KeyPath, OptionalKeyPath, WritableKeyPath, WritableOptionalKeyPath};
+use rust_keypaths::{KeyPath, WritableKeyPath};
 
 struct Garage {
     cars: Vec<String>,
 }
 
 fn main() {
-    let kp: KeyPath<Garage, Vec<String, impl for<\'r> Fn(&\'r Garage) -> &\'r Vec<String>> = KeyPath::new(|g: &Garage| &g.cars);
+    let kp = KeyPath::new(|g: &Garage| &g.cars);
     let mut g = Garage {
         cars: vec!["BMW".into(), "Tesla".into(), "Audi".into()],
     };
 
     // Immutable iteration
     if let Some(iter) = kp.iter::<String>(&g) {
+    if let Some(iter) = kp.iter(&g) {
         for c in iter {
             println!("car: {}", c);
         }
@@ -19,7 +20,7 @@ fn main() {
 
     // Mutable iteration
     let kp_mut = WritableKeyPath::new(|g: &mut Garage| &mut g.cars);
-    if let Some(iter) = kp_mut.iter_mut::<String>(&mut g) {
+    if let Some(iter) = kp_mut.iter_mut(&mut g) {
         for c in iter {
             c.push_str(" 🚗");
         }
