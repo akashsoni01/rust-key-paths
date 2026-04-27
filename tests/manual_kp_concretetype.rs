d224614294045b68dab7822ec42b2c4cbdda739f
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2026-04-27 09:09:29 +0530

    working cpy

--- tests/manual_kp_concretetype.rs
+++ tests/manual_kp_concretetype.rs
@@ -20,14 +20,19 @@ fn rect_size_kp<'a>() -> Kp<
     &'a Size,
     &'a mut Rectangle,
     &'a mut Size,
-    impl Fn(&'a Rectangle) -> Option<&'a Size>,
-    impl Fn(&'a mut Rectangle) -> Option<&'a mut Size>
+    impl for<'b> Fn(&'b Rectangle) -> Option<&'b Size>,
+    impl for<'b> Fn(&'b mut Rectangle) -> Option<&'b mut Size>
 > {
     Kp::new(
-        |x: &'a Rectangle| { Some(& x.size) },
-        |x: &'a mut Rectangle| { Some(&mut x.size) }
+        hrtb_ref(|x: &Rectangle| { Some(& x.size) }),
+        hrtb_mut(|x: &mut Rectangle| { Some(&mut x.size) })
     )
 }
+fn hrtb_ref<T, U, F>(f: F) -> F
+where F: for<'a> Fn(&'a T) -> Option<&'a U> { f }
+
+fn hrtb_mut<T, U, F>(f: F) -> F
+where F: for<'a> Fn(&'a mut T) -> Option<&'a mut U> { f }
 
 // Manual keypath: Size -> width
 fn size_width_kp<'a>() -> Kp<
@@ -37,18 +42,18 @@ fn size_width_kp<'a>() -> Kp<
     &'a u32,
     &'a mut Size,
     &'a mut u32,
-    impl Fn(&'a Size) -> Option<&'a u32>,
-    impl Fn(&'a mut Size) -> Option<&'a mut u32>
+    impl for<'b> Fn(&'b Size) -> Option<&'b u32>,
+    impl for<'b> Fn(&'b mut Size) -> Option<&'b mut u32>
 > {
     Kp::new(
-        |x: &Size| { Some(& x.width) },
-        |x: &mut Size| { Some(&mut x.width) }
+        hrtb_ref(|x: &Size| { Some(& x.width) }),
+        hrtb_mut(|x: &mut Size| { Some(&mut x.width) })
     )
 }
 
 #[test]
 fn manual_keypath_then_read_write_works() {
-    let rect = Rectangle {
+    let mut rect = Rectangle {
         size: Size {
             width: 30,
             height: 50,
@@ -56,16 +61,16 @@ fn manual_keypath_then_read_write_works() {
         name: "MyRect".to_string(),
     };
 
-    let width_kp = rect_size_kp().then(size_width_kp());
+    // let width_kp = rect_size_kp().then(size_width_kp());
 
-    println!("size of concreate kp = {:?}", size_of_val(&width_kp));
-    assert_eq!(width_kp.get(&rect), Some(&30));
+    println!("size of concreate kp = {:?}", size_of_val(&rect_size_kp().then(size_width_kp())));
+    assert_eq!(rect_size_kp().then(size_width_kp()).get(&rect), Some(&30));
 
-    // if let Some(w) = width_kp.get_mut(&mut rect) {
-    //     *w += 12;
-    // }
+    if let Some(w) = rect_size_kp().then(size_width_kp()).get_mut(&mut rect) {
+        *w += 12;
+    }
 
-    // assert_eq!((width_kp).get(&rect), Some(&42));
+    assert_eq!((rect_size_kp().then(size_width_kp())).get(&rect), Some(&42));
     // assert_eq!(rect.size.height, 50);
     // assert_eq!(rect.name, "MyRect");
 }
